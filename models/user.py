adfb9a5350624720039961d0281437fbaa50df92
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-07 23:22:20 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,22 @@
 #!/usr/bin/bash
+''' module - user
+This is the user model for my application'''
+
+
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    '''
+    BaseModel Inherited
+    Public class attributes:
+        - email
+        - password
+        - first_name
+        - last_name
+        '''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
