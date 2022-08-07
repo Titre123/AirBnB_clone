806133f48692b149889d45783f02367e89099fd5
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-07 23:16:47 +0100

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,16 @@
 #!/usr/bin/bash
+''' module - amenity
+This is the amenity model for my application'''
+
+
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    BaseModel Inherited
+
+    Public class attributes:
+        - name
+    '''
+    name = ""
