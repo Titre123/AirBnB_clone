3485b00f357f337d9b6876e3ed38181d47de9e56
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-07 23:17:51 +0100

    place

--- models/place.py
+++ models/place.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+''' module - place
+This is the place model for my application'''
+
+
+from base_model import BaseModel
+
+
+class Place(BaseModel):
+    '''
+    BaseModel Inherited
+    Public class attributes:
+        - city_id
+    '''
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
