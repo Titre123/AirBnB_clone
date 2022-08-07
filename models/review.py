ac3951603e7321bc4312d62c51b5cf7383a498f2
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-07 23:20:48 +0100

    review

--- models/review.py
+++ models/review.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+''' module - place
+This is the place model for my application'''
+
+
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    '''
+    BaseModel Inherited
+    Public class attributes:
+        - place_id
+        - user_id
+        - text
+    '''
+    place_id = ""
+    user_id = ""
+    text = ""
