3e05349a5657fb3d23f33fbf09bf278af61c4615
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-07 23:17:06 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+''' module - city
+This is the city model for my application'''
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''
+    BaseModel Inherited
+    Public class attributes:
+        - state_id
+        - name
+    '''
+    state_id = ""
+    name = ""
