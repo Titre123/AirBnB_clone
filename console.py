93f7da32eaeacb09d5c00a60fdee057ec7f9e714
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-08 18:23:34 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+'''
+module = console
+contains the entry point of the command interpreter'''
+
+
+import cmd
+from models.base_model import BaseModel
+from models import storage
+import re
+
+
+class HBNBCommand(cmd.Cmd):
+    '''
+    class for command
+    '''
+
+    prompt: str = "(hbnb) "
+
+    def do_quit(self, line):
+        '''Quit command to exit the program'''
+        return True
+
+    def help_quit(self):
+        '''Prints out how to use the quit command'''
+        print("Quit command to exit or quit the program")
+
+    def help_EOF(self):
+        '''Prints out how to use the EOF command'''
+        print("Exits or quit the program with the EOF command")
+
+    def do_EOF(self, line):
+        '''exit the program'''
+        return True
+
+    def emptyline(self):
+        '''an empty line + ENTER shouldn't execute anything'''
+        return cmd.Cmd.emptyline(self)  # or Super().emptyline()
